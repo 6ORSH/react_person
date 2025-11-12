88033e5186e634c2f26439ba0b8be08cd1250d0f
Nikita Bedenko <nikitabedenko@gmail.com>
2025-11-12 21:37:21 +0200

    add solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,19 @@
-// export const Person = ({ person }) => ();
+function getParnterStatus(sex, isMarried, partnerName) {
+  if (!isMarried) {
+    return <p className="Person__partner">I am not married</p>;
+  }
+
+  return (
+    <p className="Person__partner">
+      {partnerName} is my {sex === 'm' ? 'wife' : 'husband'}
+    </p>
+  );
+}
+
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">My name is {person.name}</h2>
+    {person.age > 0 && <p className="Person__age">I am {person.age}</p>}
+    {getParnterStatus(person.sex, person.isMarried, person.partnerName)}
+  </section>
+);
