aa0bfcd0f5449577539029e39d4fc3a54e888bc2
Nikita Bedenko <nikitabedenko@gmail.com>
2025-11-12 22:15:29 +0200

    fix review issues

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,19 +1,28 @@
-function getParnterStatus(sex, isMarried, partnerName) {
+function getPartnerStatus(sex, isMarried, partnerName) {
   if (!isMarried) {
     return <p className="Person__partner">I am not married</p>;
   }
 
   return (
     <p className="Person__partner">
-      {partnerName} is my {sex === 'm' ? 'wife' : 'husband'}
+      {partnerName} is my{' '}
+      {
+        sex === 'm'
+          ? 'wife'
+          : 'husband'
+      }
     </p>
   );
 }
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">My name is {person.name}</h2>
-    {person.age > 0 && <p className="Person__age">I am {person.age}</p>}
-    {getParnterStatus(person.sex, person.isMarried, person.partnerName)}
-  </section>
-);
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
+      {getPartnerStatus(sex, isMarried, partnerName)}
+    </section>
+  );
+};
